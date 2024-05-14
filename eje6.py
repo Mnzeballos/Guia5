bf088e19faba6c0a88e7fa1c8efda2da9f131876
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 11:49:56 -0300

    defino las funciones

---
+++ eje6.py
@@ -0,0 +1,12 @@
+# Funciones de la calculadora
+def suma(num1, num2):
+    return num1 + num2
+
+def resta(num1, num2):
+    return num1 - num2
+
+def multiplicacion(num1, num2):
+    return num1 * num2
+
+def division(num1, num2):
+    return num1 / num2
