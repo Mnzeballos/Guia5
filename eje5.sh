005493f6755e33c3bc2db6caf454e5c590d05170
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 10:35:33 -0300

    eje5 v2 - programa py v2 y sh

---
+++ eje5.sh
@@ -0,0 +1,1 @@
+python3 eje5a.py
--- eje5a.py
+++ eje5a.py
@@ -1,7 +1,7 @@
 import time
 import sys
-print("Contar hasta")
-n=int(input())
+#print("Contar hasta")
+#n=int(input())
 def contador(n):
     for i in range(1, n):
         print(i)
