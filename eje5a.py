18da604fff158f3b50abf4b7b81006c63170c2c2
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 10:57:43 -0300

    eje5 v4 - simplificación y mejora del py

--- eje5a.py
+++ eje5a.py
@@ -2,13 +2,6 @@ import time
 import sys
 print("Contar hasta")
 n=int(input())
-def contador():
-    for i in range(1, n+1):
-        print(i)
-        time.sleep(1)
-#if __name__ == "__main__":
-#   if len(sys.argv) != 2:
-#        print("Uso: python contador3.py <número>")
-#        sys.exit(1)
-#    n = int(sys.argv[1])
-#    contador(n)
+for i in range(1, n+1):
+    print(i)
+    time.sleep(1)
