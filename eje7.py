7f312f2b84a547408ad5426e7ef3e91fee728aa4
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 15:17:42 -0300

    eje7 v2 - memoria RAM

--- eje7.py
+++ eje7.py
@@ -2,6 +2,29 @@
 Cree un programa en Python que imprima la hora del sistema, estado de memoria
 RAM y almacenamiento disponible en la partición montada en “/”.
 '''
-import datetime
 import time
+import os
+#import commands
+#import sys
+#import getop
+import psutil
+
+## Hora
 print("La hora es:", time.strftime("%H:%M:%S", time.localtime()))
+
+## Memoria
+#def checkmem():
+#	    result1 = commands.getoutput('free|grep Mem:|tr -s "'" "'" |cut -d "'" "'" -f 2')
+#	    result2 = commands.getoutput('free|grep Mem:|tr -s "'" "'" |cut -d "'" "'" -f 3')
+#	    result3=int(result2)*100/int(result1)
+#	    return result3
+#mem=checkmem()
+
+# Para convertir bytes en Gigabytes
+def bytes_to_GB(bytes):
+    gb = bytes/(1024*1024*1024)
+    gb = round(gb, 2)
+    return gb
+#memoria RAM
+mem= psutil.virtual_memory()
+print("\n---RAM disponible:", bytes_to_GB(mem.available), "Gb")
