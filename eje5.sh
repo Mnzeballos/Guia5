24874cac58ca3c963d070389cfc6390cc797f157
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 11:00:01 -0300

    los 3 programas de py

--- eje5.sh
+++ eje5.sh
@@ -1,3 +1,3 @@
 python3 eje5a.py
-#python3 eje5b.py
-#python3 eje5c.py
+python3 eje5b.py
+python3 eje5c.py
---
+++ eje5b.py
@@ -0,0 +1,7 @@
+import time
+import sys
+print("Contar hasta")
+n=int(input())
+for i in range(1, n+1):
+    print(i)
+    time.sleep(1)
