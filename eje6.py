88dc915abc10af4129ee55e2619c892ec1ba53c3
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 11:54:11 -0300

    eje6 v2 - entrada de valores y operadores

--- eje6.py
+++ eje6.py
@@ -10,3 +10,10 @@ def multiplicacion(num1, num2):
 
 def division(num1, num2):
     return num1 / num2
+# Introducción dos valores
+print("Número 1")
+num1=float(input())
+print("operación:")
+op=input()
+print("Número 2")
+num2=float(input())
