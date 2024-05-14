60a3a9d2bcb82da8a00afc37b962f6b98e16aac5
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 10:50:53 -0300

    eje5 v3 - py v3

--- eje5a.py
+++ eje5a.py
@@ -1,8 +1,14 @@
 import time
 import sys
-#print("Contar hasta")
-#n=int(input())
-def contador(n):
-    for i in range(1, n):
+print("Contar hasta")
+n=int(input())
+def contador():
+    for i in range(1, n+1):
         print(i)
         time.sleep(1)
+#if __name__ == "__main__":
+#   if len(sys.argv) != 2:
+#        print("Uso: python contador3.py <número>")
+#        sys.exit(1)
+#    n = int(sys.argv[1])
+#    contador(n)
