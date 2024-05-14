30e35d5926ac4b2a0fcb60807a5a1cd09a681a84
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 11:49:29 -0300

    eje6

--- eje5.sh
+++ eje5.sh
@@ -1,7 +1,10 @@
 echo "para correr los programas de forma secuencial:"
-python3 eje5a.py 10 
-python3 eje5b.py 11 
-python3 eje5c.py 12
+python3 eje5a.py 10 &
+echo "PID $PID1=$eje5a"
+python3 eje5b.py 11 &
+echo "PID $PID1=$!"
+python3 eje5c.py 12 &
+echo "PID $PID1=$!"
 
 echo "para correr los programas de forma simultanea:"
 python3 eje5a.py 10 &
