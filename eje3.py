3dcdbf61e8b83e08f286f8ce5fd4f4443cc3e5f3
Manuel Zeballos <manuel.zeballos@ig.edu.ar>
2024-05-14 09:56:14 -0300

    eje3 v2 - funcion if

--- eje3.py
+++ eje3.py
@@ -4,11 +4,11 @@
 #1 sueldo x 0.83
 #2 sueldo x 1.2
 #3 sueldo x 5
-sueldo=
-rango=
-if rango==1:
+sueldo=2
+rango=1
+if rango == 1:
     print("El sueldo es:",sueldo*0.83,"para el rango: 1")
-elif rango==2:
+elif rango == 2:
     print("El sueldo es:",sueldo*1.2,"para el rango: 2")
-else rango==3:
+else:
     print("El sueldo es:",sueldo*5,"para el rango: 3")
